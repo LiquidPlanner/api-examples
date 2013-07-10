283aa33f43dffd60323c904327004c5014a0163b
Jon Snelling <jon@liquidplanner.com>
2013-07-10 14:10:07 -0700

    C#.net and VB.net examples

---
+++ c#.net/LiquidPlanner/LiquidPlanner/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+/*
+ * LiquidPlanner
+ * 
+ * Uses the JSON.net library
+ * available at: http://json.codeplex.com/releases/view/89222
+ * 
+ */
+
+namespace LiquidPlanner
+{
+    class LpResponse 
+    {
+        public String response { get; set; }
+        public Exception error { get; set; }
+    }
+
+    class LpObject
+    {
+        public Int32 id { get; set; }
+    }
+
+    class Member : LpObject
+    {
+        public String name       { get; set; }
+        public String first_name { get; set; }
+        public String last_name  { get; set; }
+
+        public override String ToString()
+        {
+            return "User: "+this.id + "> " + this.name + "> " + this.first_name + " " + this.last_name;
+        }
+    }
+    class Workspace : LpObject
+    {
+        public String name { get; set; }
+        
+        public override String ToString()
+        {
+            return "Space: " + this.id + "> " + this.name;
+        }
+    }
+    class Item : LpObject
+    {
+        public String name      { get; set; }
+        public String type      { get; set; }
+        public Int32  owner_id  { get; set; }
+        public Int32  parent_id { get; set; }
+
+        public override String ToString()
+        {
+            return "Item: " + this.id +"("+ this.type + ")> " + this.name;
+        }
+    }
+
+    class LiquidPlanner
+    {
+        public  string  Hostname { get; set; }
+
+        private String _password;
+
+        public  String  Username    { get; set; }
+        public  String  Password    { get { return null; } set { _password = value; } }
+        public  Int32   WorkspaceId { get; set; }
+
+        public LiquidPlanner(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        private LpResponse request(String verb, String url, Object data)
+        {
+            HttpWebRequest request;
+            WebResponse response;
+            String      uri;
+            LpResponse  lp_response;
+
+            uri = "https://app.liquidplanner.com/api" + url;           
+
+            request = WebRequest.Create(uri) as HttpWebRequest;
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = verb;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.Headers.Set("Authorization", Convert.ToBase64String(Encoding.ASCII.GetBytes(this.Username + ":" + this._password)));
+            
+            if (null != data) {
+                request.ContentType = "application/json";
+                String jsonPayload = JsonConvert.SerializeObject(data);
+                byte[] jsonPayloadByteArray = Encoding.ASCII.GetBytes(jsonPayload.ToCharArray());
+                request.GetRequestStream().Write(jsonPayloadByteArray, 0, jsonPayloadByteArray.Length);
+            }
+
+
+            lp_response = new LpResponse();
+            try
+            {
+                response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                lp_response.response = reader.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                lp_response.error = e;
+            }
+            return lp_response;
+        }
+
+        public LpResponse get(String url)
+        {
+            return request("GET", url, null);
+        }
+        public LpResponse post(String url, Object data)
+        {
+            return request("POST", url, data);
+        }
+        public LpResponse put(String url, Object data)
+        {
+            return request("PUT", url, data);
+        }
+
+        public t GetObject<t>(LpResponse response) {
+            if (null != response.error)
+                throw response.error;
+            return JsonConvert.DeserializeObject<t>(response.response);
+        }
+
+        public Member GetAccount()
+        {
+            return GetObject<Member>(get("/account"));
+        }
+        public List<Workspace> GetWorkspaces()
+        {
+            return GetObject<List<Workspace>>(get("/workspaces"));
+        }
+        public List<Item> GetProjects()
+        {
+            return GetObject<List<Item>>(get("/workspaces/" + this.WorkspaceId + "/projects"));
+        }
+        public List<Item> GetTasks()
+        {
+            return GetObject<List<Item>>(get("/workspaces/" + this.WorkspaceId + "/tasks"));
+        }
+
+        public Item CreateTask(LpObject data)
+        {
+            return GetObject<Item>(post("/workspaces/" + this.WorkspaceId + "/tasks", new
+            {
+                task = data
+            }));
+        }
+        public Item UpdateTask(LpObject data)
+        {
+            return GetObject<Item>(put("/workspaces/" + this.WorkspaceId + "/tasks/" + data.id, new
+            {
+                task = data
+            }));
+        }
+    }
+
+    class CommandLineDemo
+    {
+        static void Main(string[] args)
+        {
+            String username, password;
+            if (args.Length < 2)
+            {
+                Console.Write("Enter email address: ");
+                username = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+            }
+            else
+            {
+                username = args[0];
+                password = args[1];
+            }
+
+            LiquidPlanner liquidplanner = new LiquidPlanner(username, password);
+            
+            Member myAccount = liquidplanner.GetAccount();
+            Console.WriteLine(myAccount.ToString());
+
+            List<Workspace> spaces = liquidplanner.GetWorkspaces();
+            foreach (Workspace space in spaces)
+            {
+                Console.WriteLine(space.ToString());
+            }
+
+            liquidplanner.WorkspaceId = spaces[0].id;
+            List<Item> firstSpaceTasks = liquidplanner.GetTasks();
+            foreach (Item task in firstSpaceTasks)
+            {
+                Console.WriteLine(task.ToString());
+            }
+
+            List<Item> firstSpaceProjects = liquidplanner.GetProjects();
+            foreach (Item task in firstSpaceProjects)
+            {
+                Console.WriteLine(task.ToString());
+            }
+
+            //Create a new - unassigned task -in the first project of the first space.
+            Item aTask = liquidplanner.CreateTask(new Item()
+            {
+                name = "My new Task Name",
+                parent_id = firstSpaceProjects[0].id
+            });
+
+            //Now assign the task to you.
+            aTask.owner_id = myAccount.id;
+            aTask = liquidplanner.UpdateTask(aTask);
+        }
+    }
+}
---
+++ vb.net/LiquidPlannerVB/LiquidPlanner.vb
@@ -0,0 +1,192 @@
+﻿Imports System.Net          ' provides WebRequest et al
+Imports Newtonsoft.Json
+Imports System.Text         ' provides Encoding
+Imports System.IO           ' provides Stream
+
+
+' LiquidPlanner
+'
+' Uses the JSON.net library
+' available at: http://json.codeplex.com/releases/view/89222
+
+Module LiquidPlanner
+
+    Class LpResponse
+        Property response As String
+        Property anError As Exception
+    End Class
+
+    Class LpObject
+        Property id As Integer
+    End Class
+
+    Class Member
+        Inherits LpObject
+
+        Property name As String
+        Property first_name As String
+        Property last_name As String
+
+        Overrides Function ToString() As String
+            Return "User: " & id & "> " & name & "> " & first_name & " " & last_name
+        End Function
+    End Class
+
+    Class Workspace
+        Inherits LpObject
+
+        Property name As String
+
+        Public Overrides Function ToString() As String
+            Return "Space: " & id & "> " & name
+        End Function
+    End Class
+
+    Class Item
+        Inherits LpObject
+
+        Property name As String
+        Property type As String
+        Property owner_id As Integer
+        Property parent_id As Integer
+
+        Public Overrides Function ToString() As String
+            Return "Item: " & id & "(" & type & ")> " & name
+        End Function
+    End Class
+
+    ' api connection class
+    Class LiquidPlanner
+        Private _password As String
+
+        Property Username As String
+        WriteOnly Property Password As String
+            Set(ByVal value As String)
+                _password = Value
+            End Set
+        End Property
+        Property WorkspaceId As Integer
+
+        Public Sub New(ByVal theUsername As String, ByVal thePassword As String)
+            Username = theUsername
+            Password = thePassword
+        End Sub
+
+        Private Function request(ByVal verb As String, ByVal url As String, ByRef data As Object) As LpResponse
+            Dim theRequest As HttpWebRequest
+            Dim response As WebResponse
+            Dim uri As String
+            Dim lp_response As LpResponse
+
+            uri = "https://app.liquidplanner.com/api" & url
+
+            theRequest = WebRequest.Create(uri)
+            theRequest.AutomaticDecompression = DecompressionMethods.Deflate Or DecompressionMethods.GZip
+            theRequest.Credentials = CredentialCache.DefaultCredentials
+            theRequest.Method = verb
+            theRequest.Headers.Set("Authorization", Convert.ToBase64String(Encoding.ASCII.GetBytes(Username + ":" + _password)))
+
+            If Not IsNothing(data) Then
+                theRequest.ContentType = "application/json"
+                Dim jsonPayload As String = JsonConvert.SerializeObject(data)
+                Dim jsonPayloadByteArray = Encoding.ASCII.GetBytes(jsonPayload.ToCharArray)
+                theRequest.GetRequestStream.Write(jsonPayloadByteArray, 0, jsonPayloadByteArray.Length)
+            End If
+
+            lp_response = New LpResponse()
+            Try
+                response = theRequest.GetResponse
+                Dim reader As StreamReader = New StreamReader(response.GetResponseStream)
+                lp_response.response = reader.ReadToEnd
+            Catch ex As Exception
+                lp_response.anError = ex
+            End Try
+
+            Return lp_response
+        End Function
+
+        Public Function DoGet(ByVal url As String) As LpResponse
+            Return request("GET", url, Nothing)
+        End Function
+        Public Function DoPost(ByVal url As String, ByRef data As Object)
+            Return request("POST", url, data)
+        End Function
+        Public Function DoPut(ByVal url As String, ByRef data As Object)
+            Return request("PUT", url, data)
+        End Function
+
+        Public Function GetObject(Of t)(ByRef response As LpResponse) As t
+            If Not IsNothing(response.anError) Then
+                Throw response.anError
+            End If
+            Return JsonConvert.DeserializeObject(Of t)(response.response)
+        End Function
+
+        Public Function GetAccount() As Member
+            Return GetObject(Of Member)(DoGet("/account"))
+        End Function
+        Public Function GetWorkspaces() As List(Of Workspace)
+            Return GetObject(Of List(Of Workspace))(DoGet("/workspaces"))
+        End Function
+        Public Function GetProjects() As List(Of Item)
+            Return GetObject(Of List(Of Item))(DoGet("/workspaces/" & WorkspaceId & "/projects"))
+        End Function
+        Public Function GetTasks() As List(Of Item)
+            Return GetObject(Of List(Of Item))(DoGet("/workspaces/" & WorkspaceId & "/tasks"))
+        End Function
+
+        Public Function CreateTask(ByRef data As LpObject) As Item
+            Return GetObject(Of Item)(DoPost("/workspaces/" & WorkspaceId & "/tasks", New With {Key .task = data}))
+        End Function
+
+        Public Function UpdateTask(ByRef data As LpObject) As Item
+            Return GetObject(Of Item)(DoPut("/workspaces/" & WorkspaceId & "/tasks/" & data.id, New With {Key .task = data}))
+        End Function
+
+    End Class
+
+
+    Sub Main()
+        Dim liquidplanner As LiquidPlanner
+        Dim username As String
+        Dim password As String
+        Dim args() As String = System.Environment.GetCommandLineArgs()
+
+        If args.Count < 2 Then
+            Console.Write("Enter email adress: ")
+            username = Console.ReadLine
+            Console.Write("Enter password: ")
+            password = Console.ReadLine
+        Else
+            username = args(1)
+            password = args(2)
+        End If
+
+        liquidplanner = New LiquidPlanner(username, password)
+
+        Dim myAccount As Member = liquidplanner.GetAccount
+        Console.WriteLine(myAccount.ToString)
+
+        Dim spaces As List(Of Workspace) = liquidplanner.GetWorkspaces
+        For Each space As Workspace In spaces
+            Console.WriteLine(space.ToString)
+        Next
+
+        liquidplanner.WorkspaceId = spaces.Item(0).id
+        Dim firstSpaceProjects As List(Of Item) = liquidplanner.GetProjects
+        For Each project As Item In firstSpaceProjects
+            Console.WriteLine(project.ToString)
+        Next
+
+        'create a new - unassigned task -in teh first project of the first space.
+        Dim aTask As Item = liquidplanner.CreateTask(New Item With {
+                                                     .name = "test",
+                                                     .parent_id = firstSpaceProjects.Item(0).id
+                                                 })
+
+        'now assign the task to you
+        aTask.owner_id = myAccount.id
+        aTask = liquidplanner.UpdateTask(aTask)
+    End Sub
+
+End Module

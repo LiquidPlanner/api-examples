c7441d2103af1f8810baab0c19ed2372eff27f74
Sephizor <sephizor@sephizor.com>
2013-08-19 10:48:32 +0100

    Add new methods for other API calls

--- java/LiquidPlanner.java
+++ java/LiquidPlanner.java
@@ -7,10 +7,15 @@ import java.net.*;
 import java.io.*;
 import javax.net.ssl.*;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
+@SuppressWarnings("rawtypes")
 public class LiquidPlanner {
+	
+  private static final int POST = 1;
+  private static final int PUT = 2;
 
   private String base_uri = "https://app.liquidplanner.com/api";
   private String email;
@@ -38,9 +43,16 @@ public class LiquidPlanner {
     this.workspace_id = workspace_id;
   }
   
-  private ArrayList<HashMap> json_to_hashmap(String json) {
+  private ArrayList<HashMap> json_to_hashmap(String json, boolean isArray) {
     JsonParser parser = new JsonParser();
-    JsonArray array = parser.parse(json).getAsJsonArray();
+    JsonArray array;
+    if(isArray) {
+    	array = parser.parse(json).getAsJsonArray();
+    }
+    else {
+    	String newString = "[" + json + "]";
+    	array = parser.parse(newString).getAsJsonArray();
+    }
     ArrayList<HashMap> list = new ArrayList<HashMap>();
     for (JsonElement element : array) {
        list.add((HashMap) gson.fromJson(element, returnType.getType()));
@@ -48,7 +60,7 @@ public class LiquidPlanner {
     return list;
   }
    
-  private ArrayList<HashMap> get(String url) {
+  private ArrayList<HashMap> get(String url, boolean isArray) {
     try{
      HttpsURLConnection connection = (HttpsURLConnection) new URL(this.base_uri + url).openConnection();
      connection.setRequestProperty("Content-Type", "application/json");
@@ -58,7 +70,7 @@ public class LiquidPlanner {
      while ( (line = br.readLine()) != null) {
       sb.append(line);
      }
-     return json_to_hashmap(sb.toString());
+     return json_to_hashmap(sb.toString(), isArray);
     } catch (MalformedURLException e) {
       System.out.println("Bad URL: " + base_uri + url);
     } catch (IOException e) {
@@ -68,14 +80,28 @@ public class LiquidPlanner {
     return null;
   }
    
-  private String post(String url, String options) {
+  private String sendData(String url, String options, int sendMethod) {
     try{
       HttpsURLConnection connection = (HttpsURLConnection) new URL(this.base_uri + url).openConnection();
       connection.setDoOutput(true);
       connection.setRequestProperty("Content-Type", "application/json");
+      if(sendMethod == POST) {
+    	connection.setRequestMethod("POST");
+      }
+      else if(sendMethod == PUT) {
+    	connection.setRequestMethod("PUT");  
+      }
       OutputStream out = connection.getOutputStream();
       out.write(options.getBytes());
-      BufferedReader br = new BufferedReader( new InputStreamReader(connection.getInputStream()));
+      String encoding = connection.getHeaderField("Content-Encoding");
+      boolean isGzip = false;
+      if(encoding != null)
+    	  isGzip = Pattern.matches("gzip", encoding);
+      InputStream inputStream = connection.getInputStream();
+      if (isGzip) {
+        inputStream = new GZIPInputStream( inputStream );
+      }
+      BufferedReader br = new BufferedReader( new InputStreamReader( inputStream ) );
       String line;
       StringBuilder sb = new StringBuilder();
       while ( (line = br.readLine()) != null) {
@@ -91,28 +117,44 @@ public class LiquidPlanner {
     return null;
   }
   
-  //gets account information about the current user
   public ArrayList<HashMap> account() {
-    return get("/account");
+    return get("/account", true);
   }
   
-  //gets information about all workspaces of which the current user is a member
   public ArrayList<HashMap> workspaces() {
-    return get("/workspaces");
+    return get("/workspaces", true);
   }
   
-  //gets information about all projects in the current workspace
   public ArrayList<HashMap> projects() {
-    return get("/workspaces/" + this.workspace_id.toString() + "/projects");
+    return get("/workspaces/" + this.workspace_id.toString() + "/projects", true);
   }
   
-  //gets informations about all tasks in the current workspace
   public ArrayList<HashMap> tasks() {
-    return get("/workspaces/" + this.workspace_id.toString() + "/tasks");
+    return get("/workspaces/" + this.workspace_id.toString() + "/tasks", true);
   }
   
-  //creates a task by POSTing data
+  public ArrayList<HashMap> comments() {
+	  return get("/workspaces/" + this.workspace_id.toString() + "/comments", true);
+  }
+  
+  public Boolean isTaskDone(int taskID) {
+	  HashMap theTask = get("/workspaces/" + this.workspace_id.toString() + "/tasks/" + taskID, false).get(0);
+	  return (Boolean) theTask.get("is_done");
+  }
+  
+  public ArrayList<HashMap> commentsForTask(int taskID) {
+	  return get("/workspaces/" + this.workspace_id.toString() + "/tasks/" + taskID + "/comments", true);
+  }
+
   public String createTask(String task) {
-    return post("/workspaces/" + this.workspace_id.toString() + "/tasks", "{\"task\": " + task + "}");
+	  return sendData("/workspaces/" + this.workspace_id.toString() + "/tasks", "{\"task\": " + task + "}", POST);
+  }
+
+  public String createComment(int itemID, String comment) {
+	  return sendData("/workspaces/" + this.workspace_id.toString() + "/tasks/" + itemID + "/comments", "{\"comment\": " + comment + "}", this.POST);
+  }
+
+  public String updateTask(String task, int taskID) {
+	  return sendData("/workspaces/" + this.workspace_id.toString() + "/tasks/" + taskID, "{\"task\": " + task + "}", PUT);
   }
 }

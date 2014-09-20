63d36cc814954b801bd794a873e11c6b0dad6a0e
Jon Snelling <jon@liquidplanner.com>
2014-09-20 11:10:56 -0400

    ruby multiple owner example

--- ruby/liquidplanner.rb
+++ ruby/liquidplanner.rb
@@ -36,33 +36,63 @@ class LiquidPlanner
     get('/account')
   end
 
+  def help
+    get('/help.json')
+  end
+
+  def workspace_url
+    "/workspaces/#{workspace_id}"
+  end
+
   def workspaces
     get('/workspaces')
   end
 
-  def projects
-    get("/workspaces/#{workspace_id}/projects")
+  def members
+    get("#{workspace_url}/members")
   end
 
-  def tasks
-    get("/workspaces/#{workspace_id}/tasks")
+  def teams
+    get("#{workspace_url}/teams")
+  end
+
+  def get_treeitem id
+    get("#{workspace_url}/treeitems/#{id}")
+  end
+
+  def projects
+    get("#{workspace_url}/projects")
+  end
+
+  def tasks query = nil
+    get("#{workspace_url}/tasks#{query}")
   end
 
   def create_task(data)
-    options = { :body => { :task => data } }
-    post("/workspaces/#{workspace_id}/tasks", :body => { :task => data })
+    post("#{workspace_url}/tasks", :body => { :task => data })
   end
 
   def update_task(data)
-    options = { :body => { :task => data } }
-    put("/workspaces/#{workspace_id}/tasks/#{data['id']}", :body => { :task => data })
+    put("#{workspace_url}/tasks/#{data['id']}", :body => { :task => data })
   end
 
-  def self.demo
+  def events query = nil
+    get("#{workspace_url}/events#{query}")
+  end
+
+  def update_assignment ti_id, assignment
+    post("#{workspace_url}/treeitems/#{ti_id}/update_assignment", :body => assignment)
+  end
+
+  def self.connection
     email    = ask("LiquidPlanner email: ")
     password = ask("LiquidPlanner password for #{email}: ") {|q| q.echo = false}
 
-    lp = LiquidPlanner.new(email, password)
+    LiquidPlanner.new(email, password)
+  end
+
+  def self.demo
+    lp = connection
 
     account = lp.account
     puts "You are #{account['user_name']} (#{account['id']})"
@@ -90,10 +120,148 @@ class LiquidPlanner
 
   end
 
+  def self.demo_multiple_owners
+    lp = connection
+
+    workspaces = lp.workspaces
+    ws         = workspaces.first
+
+    lp.workspace_id = ws['id']
+
+    MultipleOwnerDemo.new lp
+  end
+
+end
+
+class MultipleOwnerDemo
+  attr_accessor :lp
+  attr_accessor :members
+  attr_accessor :teams
+
+  def initialize( lp ) 
+    self.lp = lp
+    demo 
+  end
+
+  def members_indexed_by_id
+    lp.members.inject( {} ) do |index, member| 
+      index[ member[ 'id' ] ] = member
+      index
+    end
+  end
+
+  def teams_indexed_by_id
+    lp.teams.inject( {} ) do |index, team| 
+      index[ team[ 'id' ] ] = team
+      index
+    end
+  end
+
+  def assignments_for ti
+    ti[ 'assignments' ]
+  end
+
+  def owners_for ti
+    assignments_for( ti ).map do |assignment|
+      members[ assignment[ 'person_id' ] ] || 
+      teams[ assignment[ 'team_id' ] ] 
+    end
+  end
+
+  def name_for owner
+    case owner[ 'type' ];
+      when "Member" then owner[ 'user_name' ]
+      when "Team"   then owner[ 'name' ]
+    end
+  end
+
+  def inspect_ownership ti
+    id = ti[ 'id' ].to_s.rjust( 6 )
+    name = ti[ 'name' ]
+    owners = owners_for( ti ).map{ |e| name_for e }
+
+    puts "#{ id }: #{ name } => #{ owners }"
+  end
+
+  def demo
+
+    help = lp.help
+
+    unless help[ "Task" ] && help[ "Task" ][ "assignments" ]
+      puts "LiquidPlanner does not yet support multiple owners"
+      exit
+    end
+
+    self.members = members_indexed_by_id
+    self.teams = teams_indexed_by_id
+
+    two_tasks  = lp.tasks( "?limit=2" )
+    two_events = lp.events( "?limit=2" )
+
+    if two_tasks.length < 2 || two_events.length < 2
+      puts "Make sure you have at least two tasks and at least two events."
+      exit
+    end
+
+    puts "We're going to swap ownership for the first two tasks, and the " +
+         "two events: \n"
+
+    puts "Tasks: (before)"
+    two_tasks.each { |ti| inspect_ownership ti }
+    puts "Events: (before)"
+    two_events.each { |ti| inspect_ownership ti }
+
+    # Swap the task owners
+
+    assignment_1, assignment_2 = two_tasks.map { |task| 
+      assignments_for( task ).first
+    }
+
+    two_tasks[0] = lp.update_assignment( two_tasks[0][ 'id' ], {
+      :assignment_id => assignment_1[ 'id' ],
+      :person_id     => assignment_2[ 'person_id' ],
+      :team_id       => assignment_2[ 'team_id' ],
+    })
+
+    two_tasks[1] = lp.update_assignment( two_tasks[1][ 'id' ], {
+      :assignment_id => assignment_2[ 'id' ],
+      :person_id     => assignment_1[ 'person_id' ],
+      :team_id       => assignment_1[ 'team_id' ],
+    })
+
+    # Swap the event owners
+
+    assignment_1, assignment_2 = two_events.map { |task| 
+      owners_for( task ).inject( {
+        :person_ids => [],
+        :team_ids => []
+      } ) do |ids, owner| 
+        case owner[ 'type' ];
+          when "Member" then ids[ :person_ids ] << owner[ 'id' ]
+          when "Team"   then ids[ :team_ids ] << owner[ 'id' ]
+        end
+        ids
+      end
+    }
+
+    two_events[0] = lp.update_assignment( two_events[0][ 'id' ], assignment_2 )
+    two_events[1] = lp.update_assignment( two_events[1][ 'id' ], assignment_1 )
+
+    puts "\n\nTasks: (after)"
+    two_tasks.each { |ti| inspect_ownership ti }
+    puts "Events: (after)"
+    two_events.each { |ti| inspect_ownership ti }
+
+  end
+
 end
 
 if $0 == __FILE__
   require 'highline/import'
-  LiquidPlanner.demo
+  if ARGV.length == 1 && ARGV[ 0 ] == "owners"
+    LiquidPlanner.demo_multiple_owners
+  else
+    LiquidPlanner.demo
+  end
 end
 
